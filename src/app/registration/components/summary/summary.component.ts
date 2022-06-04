6d94a22b8576917dcc45bb9776892a841eb3d816
Alireza Razinejad <a.razinejad@gmail.com>
2022-06-05 00:43:33 +0430

    Add debugging solution to view the summary at a time
    Signed-off-by: Alireza Razinejad <a.razinejad@gmail.com>

--- src/app/registration/components/summary/summary.component.ts
+++ src/app/registration/components/summary/summary.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {IRegistration} from '../../interfaces';
 
 @Component({
   selector: 'app-summary',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./summary.component.scss']
 })
 export class SummaryComponent implements OnInit {
+  @Input() registration: IRegistration | undefined;
 
   constructor() { }
 
